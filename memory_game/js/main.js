1f026603212336a86f92c191f6ac095884f15a55
Hayden Bradfield <hbradfield20@gmail.com>
2019-11-30 14:44:56 -0500

    creating game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log('up and running');
+
+var cardOne = 'king', 
+cardTwo = 'queen', 
+cardThree = 'king', 
+cardFour = 'queen';
+
+console.log('User flipped ' + cardOne);
+console.log('User flipped ' + cardTwo);
